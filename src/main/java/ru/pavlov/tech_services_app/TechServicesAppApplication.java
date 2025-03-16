a22b9fa8cbed09ab96543e874f57cc9304b68e03
Илюша <79216023478@yandex.ru>
2025-03-16 17:49:47 +0300

    Spring Initializr

---
+++ src/main/java/ru/pavlov/tech_services_app/TechServicesAppApplication.java
@@ -0,0 +1,13 @@
+package ru.pavlov.tech_services_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechServicesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechServicesAppApplication.class, args);
+	}
+
+}
